84ac49bf54a8c4a3f40a1799496a5f75ca587275
laiy <lrjoshi@ucsc.edu>
2022-04-19 14:24:06 -0700

    Create lab.js

---
+++ "js/lab.js
@@ -0,0 +1,22 @@
+/**
+ * Author: LAIY JOSHI
+ * Created:   19.04.2024
+ *
+ * (c) Copyright by Blub Corp.
+ **/
+
+//defines variables
+carName=Taurus;
+carModel=Ford;
+carcolor=Black;
+caryear=1995;
+
+//calculate
+age=2022-1995;
+
+//output
+document.writeln("carName: "+ carName+ "<br>");
+document.writeln("carModel: "+ carModel+ "<br>");
+document.writeln("carcolor: "+ carcolor+ "<br>");
+document.writeln("caryear: "+ carYear+ "<br>");
+document.writeln("carAge: "+ carAge+ "years<br>");
