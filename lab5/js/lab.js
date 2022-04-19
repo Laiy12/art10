0a62e54306c8f7588ff498e4699cf68e71046a81
laiy <lrjoshi@ucsc.edu>
2022-04-19 14:30:19 -0700

    Update lab.js

---
+++ lab5/js/lab.js
@@ -0,0 +1,22 @@
+/**
+ * Author:    laiy joshi
+ * Created:   04.19.2022
+ * License: public Domain
+**/
+
+
+//Define Variables//
+    var make = "Toyota";
+    var model = "Prius";
+    var color = "Purple";
+    var year = 1995;
+
+  // calculate//
+      age = 2022 - year;
+
+  //output//
+    document.writeln("make: " + make + "<br>");
+    document.writeln("model: " + model + "<br>");
+    document.writeln("color: " + color + "<br>");
+    document.writeln("year: " + year + "<br>");
+    document.writeln("Age: "+ age + "<br>");
