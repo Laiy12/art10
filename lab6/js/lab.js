1859fc74a0ec96c265dc942f8ef0efb1bbd07341
laiy <lrjoshi@ucsc.edu>
2022-04-25 19:42:36 -0700

    lab 6

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,25 +1,22 @@
 /**
- * Author:  Laiy joshi
- * Created:   25.04.2002
+ * Author:  Laiy joshi and Shao Chen
+ * Created:   04.25.2002
  * License: Public Domain
- * .
  **/
 
  // Define Variables//
- myTransport
-['walking', 'bus', 'car'];
+ var myTransport = ['walking', 'bus', 'car']
 
-myMainRide = {
+var myMainRide = {
   make:'Toyota',
   model: 'Prius',
   color: 'Purple',
   year: '1995',
-  age: function(){
-    return 2022 - age;
-  }
-}
+
+};
+
 
 // output//
-document.writeln("kinds of transport I use: ", myTransport, "</br>"");
+document.writeln("kinds of transport I use: ", myTransport + "</br>"");
 document.writeln("My Main Ride: <pre>",
     JSON.stringify(myMainRide, null, '\t'), "</pre>");
