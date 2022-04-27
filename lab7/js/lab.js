7d97c29e05437add7b94891ab047a9accb20be17
laiy <lrjoshi@ucsc.edu>
2022-04-27 16:49:14 -0700

    Update lab.js

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -3,6 +3,18 @@
  * Created:   04.27.2022
  * License: public Domain
  **/
-function sortUserName (){
-   
+ function SortchosenWord() {
+  var  Sortwords = window.prompt ("Hi please tell me a word");
+  console.log("cosenWord = ", chosenWord);
+  var nameArray = chosenWord.split('');
+  console.log("nameArray =", nameArray);
+  var nameArraySort = nameArray.sort();
+  console.log("nameArray =", wordsorted);
+  return wordsorted;
+  }
+
+//output//
+document.writeln ("look! your word is now sorted:",
+SortchosenWord(), "</br>");
+
 }
