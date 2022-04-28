837d261f470d5cfbc9906f0cbbe0bd3a840cb339
laiy <lrjoshi@ucsc.edu>
2022-04-27 18:08:16 -0700

    lab 7

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -4,15 +4,18 @@
  * License: public Domain
  **/
  function SortchosenWord() {
-   var chosenWord = window.prompt("Hey hey! write down any word you're thinking of in the prompt below for a surprise.");
-  console.log("chosenWord =", chosenWord);
-  var wordArray = chosenWord.split('');
-  console.log("wordArray =", wordArray);
-  var wordArraySort = wordArray.sort();
-  console.log("wordArraySort =", wordArraySort);
-  var wordSorted = wordArraySort.join('');
-  console.log("wordSorted =", wordSorted);
-  return wordSorted;
+   var chosenWord = window.prompt("Hey hey! Jot down any word you're thinking of in the prompt below for a surprise.");
+    console.log("chosenWord =", chosenWord);
+    // sort the string of letters into an array
+    var wordArray = chosenWord.split('');
+    console.log("wordArray =", wordArray);
+    // randomize array into different letters
+    var wordArraySort = wordArray.sort();
+    console.log("wordArraySort =", wordArraySort);
+    // sort the array back into a string of letters
+    var wordSorted = wordArraySort.join('');
+    console.log("wordSorted =", wordSorted);
+    return wordSorted;
 }
 
 //output//
