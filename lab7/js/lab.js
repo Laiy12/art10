ce2c43c232ad2a35ca307578123c3fc638ba99d0
laiy <lrjoshi@ucsc.edu>
2022-04-27 17:48:23 -0700

    lab 7

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -3,17 +3,18 @@
  * Created:   04.27.2022
  * License: public Domain
  **/
- function SortchosenWord() {
-   var chosenWord = window.prompt("Hey hey! write down any word you're thinking of in the prompt below for a surprise.");
-  console.log("chosenWord =", chosenWord);
-  var wordArray = chosenWord.split('');
-  console.log("wordArray =", wordArray);
-  var wordArraySort = wordArray.sort();
-  console.log("wordArraySort =", wordArraySort);
-  var wordSorted = wordArraySort.join('');
-  console.log("wordSorted =", wordSorted);
-  return wordSorted;
-  }
+
+}
+
+function main() {
+    var userName = window.prompt("Hi. Please tell me your name so I can fix it.");
+    document.writeln("<style>:root {--textlen: " + userName.length + ";}</style>");
+    var sortedName = sortUserName(userName);
+    document.writeln("Here's your sorted name: " + sortedName + "</br></br>");
+    var randomName = randomizeName(userName);
+    document.writeln("And oh hey, I fixed your name.<br>Your new one is above.");
+    document.writeln("<div class='name'>" + randomName + "</div>")
+}
 
 //output//
 document.writeln ("look! your word is now sorted:",
