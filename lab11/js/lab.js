a5a893049050c54d4af2425ee6b5baabd246a36c
laiy <lrjoshi@ucsc.edu>
2022-05-11 21:58:47 -0700

    lab 11

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -32,7 +32,7 @@ $("#r-button").click(function(){
 // change color of all button using the new button
 $(document).on('ready',function(){
 $('button').click(function() {
-   $('button').not($(this)).css('background', 'blue');
+   $('button').not($(this)).css('background', 'grey');
    $(this).css('background','none');
  });
 });
