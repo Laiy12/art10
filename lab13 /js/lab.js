35e2b8a14da0d9b124842e6e88156fa858099cb4
laiy <lrjoshi@ucsc.edu>
2022-05-19 00:37:56 -0700

    lab 12

--- lab13 /js/lab.js
+++ lab13 /js/lab.js
@@ -3,34 +3,48 @@
  * Created:   05.04.2022
  * License: public Domain
  **/
-//that takes a string as an argument: function sortingHat(str)
- function sortingHat(str) {
-//Count letters in str and assigns it to a variable length
-   len = str.length;
-//modulus (% operator) to get the remainder with 4: mod = length % 4;
-   mod = len % 4;
-//create a conditional that will return Gryffindor
- if (mod == 0) {
-     return("Gryffindor");
- }
-//create a conditional that will return Ravenclaw
- else if (mod == 1) {
-   return("Ravenclaw");
- }
- //create a conditional that will return Slytherin
- else if (mod == 2) {
-   return("Slytherin");
- }
- //create a conditional that will return Hufflepuff
- else {
-     return("Hufflepuff");
+function FizzBuzz(){
+ // Loop through numbers 1 to 200, listing them as you go
+    var oneLongString = "";
+    for (var i=1; i<200; i++) {
+   // If the number is a multiple of 7, it should print "Boom!"
+
+   // If the number is a multiple of 3 and 5, if should print "FizzBuzz!"
+     if (i % 15 == 0) {
+       console.log("FizzBuzz!");
+     oneLongString += i + "FizzBuzz!<br>";
+
+     }
+
+   // If the number is a multiple of 7 and 5, it should print ""
+     if (i % 35 == 0) {
+       console.log("BoomBuzz!");
+       oneLongString += i + "BoomBuzz!<br>";
+     }
+
+       // If the number is a multiple of 7 and 3, it should print "BoomFizz!"
+     if (i % 21 == 0) {
+       console.log("BoomFizz!");
+       oneLongString += i + "BoomFizz!<br>";
+     }
+
+     else if (i % 7 == 0) {
+       console.log("Boom!");
+        oneLongString += i + "Boom!<br>";
+     }
+     // If the number is a multiple of 5, it should print "Buzz!"
+     else if (i % 5 == 0) {
+       console.log("Buzz!");
+      oneLongString += i + "Buzz!<br>";
+     }
+     // If the number is a multiple of 3, if should print "Fizz!"
+     else if (i % 3 == 0) {
+       console.log("Fizz!");
+      oneLongString += i + "Fizz!<br>";
+     }
+     else {
+       console.log(i);
+     }
    }
+   $("#output").html(oneLongString);
  }
-//reate an click listener attached to #button
- var myButton = document.getElementById("button");
- myButton.addEventListener("click", function() {
-   var name = document.getElementById("input").value;
-   var house = sortingHat(name);
-   newText = ("You have been sorted to " + house);
-   document.getElementById("output").innerHTML = newText;
- })
