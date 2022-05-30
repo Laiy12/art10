dcff17b7fcb620098b3563f9007d603ea03af255
laiy <lrjoshi@ucsc.edu>
2022-05-30 16:44:07 -0700

    lab 15

---
+++ lab16/js/lab.js
@@ -0,0 +1,23 @@
+/**
+ * Author:   Laiy Joshi, Shao Chen
+ * Created:   05.04.2022
+ * License: public Domain
+ **/
+//The url for the request
+var endpoint = "http://numbersapi.com/random/";
+// call the function
+function getAjax(){
+//data to send, will be converted to Query string
+$.ajax({
+  url : endpoint,
+  type: "GET"
+})
+.done(function(data){
+  $("#output").html(data);
+})
+.fail(function(request, error){
+  $("output").html("something");
+})
+}
+//Add a click event to the button
+$("button").click(getAjax);
