4eec13a2c648135a90242039e43cb7a62fb9d8b8
laiy <lrjoshi@ucsc.edu>
2022-05-30 22:44:25 -0700

    lab 16

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -3,28 +3,47 @@
  * Created:   05.04.2022
  * License: public Domain
  **/
- // call the function
- function getAjax(){
- $.ajax({
-   dataType: 'json',
-   url: "https://xkcd.com/info.0.json",
-   data: {
-"month": "5",
-"num": 2626,
-"link": "",
-"year": "2022",
-"news": "",
-"safe_title": "d65536",
-"transcript": "",
-"alt": "They're robust against quantum attacks because it's hard to make a quantum system that large.",
-"img": "https://imgs.xkcd.com/comics/d65536.png",
-"title": "d65536",
-"day": "30"
-},
-   success: function(data) {
-     // do stuff
-     console.log(data);
-   }
- });
-}
-$("button").click(getAjax);
+ var endpoint = "https://xkcd.com/info.0.json";
+
+ function getAjax() {
+   $.ajax({
+       url: endpoint,
+       type: "GET",
+       dataType: 'json',
+
+
+   })
+   .done(function(data){
+     console.log("Woohoo!");
+     var month = "5";
+     var num = 2626;
+     var link = "";
+     var year = "2022";
+     var news = "";
+     var safe_title = "d65536";
+     var transcript = "";
+     var alt = "They're robust against quantum attacks because it's hard to make a quantum system that large.";
+     var img = "https://imgs.xkcd.com/comics/d65536.png";
+     var title = "d65536";
+     var day = 30;
+
+     console.log(month, num, link, year, news, safe_title, transcript);
+     $("#output").append("<h2>Month: " + month + "</h2>");
+     $("#output").append("<img src=" + img + ">");
+     $("#output").append("<h1>Number: " + num + "</h1>");
+     $("#output").append("<h1>" + link + "</h1>");
+     $("#output").append("<h1>Year: " + year + "</h1>");
+     $("#output").append("<h1>" + news + "</h1>");
+     $("#output").append("<h1>Safe Title: " + safe_title + "</h1>");
+     $("#output").append("<h1>" + transcript + "</h1>");
+     $("#output").append("<h1>Alt: " + alt + "</h1>");
+     $("#output").append("<h1>Title: " + title + "</h1>");
+     $("#output").append("<h1>Day: " + day + "</h1>");
+
+   })
+   .fail(function(request, error){
+     $("#output").html("Oops, error! Something went wrong :()");
+   })
+ }
+
+ $("#button").click(getAjax);
